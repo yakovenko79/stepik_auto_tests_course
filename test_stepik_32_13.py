d0b432a83ad6f13660455249b6d0493f3ed29b44
Михаил Яковенко <yakovenki@yandex.ru>
2023-01-24 18:28:53 +0300

    1st task for unittest

--- test_stepik_32_13.py
+++ test_stepik_32_13.py
@@ -1,21 +1,31 @@
+import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestStep1(unittest.TestCase):
-    link1 = "http://suninjuly.github.io/registration1.html"
-    link2 = "http://suninjuly.github.io/registration2.html"
+def fill_form(link):
     browser = webdriver.Chrome()
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "input.first:required").send_keys("Ivan")
+    browser.find_element(By.CSS_SELECTOR, "input.second:required").send_keys("Ivanoff")
+    browser.find_element(By.CSS_SELECTOR, "input.third:required").send_keys("aaa@aaa.aa")
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+    time.sleep(1)
+    return browser.find_element(By.TAG_NAME, "h1").text
 
+
+class TestStep1(unittest.TestCase):
     def test_page1(self):
-        self.browser.get(self.link1)
-        self.browser.find_element(By.CSS_SELECTOR, "input.first:required").send_keys("Ivan")
-        self.browser.find_element(By.CSS_SELECTOR, "input.second:required").send_keys("Ivanoff")
-        self.browser.find_element(By.CSS_SELECTOR, "input.third:required").send_keys("aaa@aaa.aa")
-        self.browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+        self.assertEqual(fill_form("http://suninjuly.github.io/registration1.html"),
+                         "Congratulations! You have successfully registered!",
+                         "registration is failed")
+
+    def test_page2(self):
+        self.assertEqual(fill_form("http://suninjuly.github.io/registration2.html"),
+                         "Congratulations! You have successfully registered!",
+                         "registration is failed")
 
 
-    def test_page(self):
-        self.browser.get(self.link2)
-        self.browser.
+if __name__ == "__main__":
+    unittest.main()
